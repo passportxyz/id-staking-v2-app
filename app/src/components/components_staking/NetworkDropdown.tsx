e7a7cbe9a79d6be879ca8546ed4c79128c0604a2
Larisa Bucur <larisabucur@192-168-0-198.rdsnet.ro>
2024-03-13 14:11:01 +0200

    update components : include network dropdown

--- app/src/components/components_staking/NetworkDropdown.tsx
+++ app/src/components/components_staking/NetworkDropdown.tsx
@@ -1,29 +1,102 @@
-import React, { ChangeEvent, useContext, useState} from "react";
+import React, { ChangeEvent, useContext, useState } from "react";
 import { Button } from "@/components/Button";
 import { Menu } from '@headlessui/react';
+import { chainConfigs, ChainConfig } from "@/utils/chains";
+import { useBalance } from 'wagmi'
 
 
+const MenuButton = ({
+  text
+}: {
+  text: string
+}) => {
 
-// Component for the dropdown menu
-//  chainConfigs
-export const NetworkDropdown = () => {
+  const [dropDownOpen, setDropDownState] = useState<boolean>(false);
+  const handleDropDown = () => {
+    setDropDownState(!dropDownOpen);
+  }
   return (
-    <Menu>
-      <Menu.Button>Current network</Menu.Button>
-      <Menu.Items>
-        {/* {options.map((option, index) => (
-          <Menu.Item key={index}>
-            {({ active }) => (
-              <button className={`${active ? 'bg-blue-500' : ''}`}>{option}</button>
-            )}
-          </Menu.Item>
-        ))} */}
-      </Menu.Items>
-    </Menu>
+    <Menu.Button as="div" className="grid grid-flow-col grid-cols-2" onClick={handleDropDown}>
+      <div>{text}</div>
+      <div className={`grid place-items-center ${dropDownOpen ? 'transform -rotate-180' : ''}`}>
+        <svg width="10" height="8" viewBox="0 0 10 8" fill="none" xmlns="http://www.w3.org/2000/svg">
+          <path d="M5.86603 7.5C5.48112 8.16667 4.51887 8.16667 4.13397 7.5L0.669873 1.5C0.284973 0.833333 0.766098 5.89981e-08 1.5359 1.26296e-07L8.4641 7.31979e-07C9.2339 7.99277e-07 9.71503 0.833334 9.33013 1.5L5.86603 7.5Z" fill="#C1F6FF" />
+        </svg>
+      </div>
+    </Menu.Button>
+  )
+}
+
+
+const MenuItem = ({chainCfg }:{chainCfg : ChainConfig}) => {
+
+
+  const balance = useBalance({
+    address: chainCfg.gtcContractAddr,
+    chainId: chainCfg.id
+  });
+
+  const [chain, setChain] = useState<number>(1);
+
+  const handleChainSelect = (chain: number) => {
+    console.log('chain - ', chain)
+    setChain(chain); // TODO: ...
+  }
+
+  return ( 
+    <Menu.Item>
+      {({ active }) => (
+        <button className={`${active ? 'bg-blue-500' : ''} grid grid-flow-col m-1`} onClick={() => handleChainSelect(chainCfg.id)}>
+          <img className="mx-2" src={chainCfg.icon} />
+          {chainCfg.label} {balance.data?.value.toString()}
+        </button>
+      )}
+     </Menu.Item>  
+  )
+    
+}
+
+export const NetworkDropdown = () => {
+
+  const [chain, setChain] = useState<string>('');
+  
+  // const result = useBalance({
+  //   address: '0xde30da39c46104798bb5aa3fe8b9e0e1f348163f',
+  //   chainId: chainId
+  // });
+
+  // const getBalance = (id: string) => {
+  //   return useBalance({
+  //       address: '0xde30da39c46104798bb5aa3fe8b9e0e1f348163f',
+  //       chainId: chainId
+  //   });
+  // }
+  // const handleChainSelect = (chain: string) => {
+  //   console.log('chain - ', chain)
+  //   setChain(chain); // TODO: ...
+  // }
+  return (
+    <div className="col-span-full grid justify-self-end grid-flow-row">
+      <Menu>
+        <MenuButton text={chain}/>
+        <Menu.Items as="div" className="grid grid-flow-row">
+          {chainConfigs.map((chain, index) => (
+            <MenuItem chainCfg={chain} />
+            // <Menu.Item key={index}>
+            //   {({ active }) => (
+            //     <button className={`${active ? 'bg-blue-500' : ''} grid grid-flow-col m-1`} onClick={setChain(chain.label)}>
+            //       <img className="mx-2" src={chain.icon} />
+            //       {chain.label} {getBalance(chain.id)}
+            //     </button>
+            //   )}
+            // </Menu.Item>
+          ))}
+        </Menu.Items>
+      </Menu>
+    </div>
   );
 };
 
 
 
 
-  
--- app/src/components/components_staking/Stake.tsx
+++ app/src/components/components_staking/Stake.tsx
@@ -9,25 +9,39 @@ const svgDropDownIcon = (
 )
 export const Stake = ({ }: any) => {
     const [inputValue, setInputValue] = useState<string>('');
-    const [dropDownOpen, setDropDownState] = useState<boolean>(false); // 
+    const [dropDownOpen, setDropDownState] = useState<boolean>(false); 
+    const [lockedPeriod, setLockedPeriodState] = useState<string>('');
 
     const handleInputChange = (event: ChangeEvent<HTMLInputElement>) => {
         setInputValue(event.target.value);
     };
 
-    const handleAddIncrement = (increment: number) => {
-        setInputValue(increment.toString());
+    const handleAddFixedValue = (value: number) => {
+        setInputValue(value.toString());
     };
 
-    const handleDropDown = (event: ChangeEvent<HTMLInputElement>) => {
+    const handleDropDown = () => {
         setDropDownState(!dropDownOpen);
     }
 
+    const handleLockedPeriod = (value: number) => {
+        setLockedPeriodState(value.toString());
+    };
+    
+    const handleStake = () => {
+        // TODO: 
+        console.log(
+            "inputValue - ", inputValue
+        );
+        console.log(
+            "lockedPeriod - ", lockedPeriod
+        )
+    };
+
     return (
-        
         <Disclosure>
-            <Disclosure.Button className={`col-span-full grid flex rounded border border-foreground-3 bg-gradient-to-b from-background to-background-4`} onClick={handleDropDown}>
-                <div className={`col-span-full grid rounded border border-foreground-3 bg-gradient-to-b from-background to-background-4`} >
+            <Disclosure.Button className={`col-span-full grid flex rounded border border-foreground-3 bg-gradient-to-r from-background  to-background-5`} onClick={handleDropDown}>
+                <div className={`col-span-full grid rounded border border-foreground-3 bg-gradient-to-r from-background to-background-5`} >
                     <div className="grid col-span-full grid-flow-col ">
                         <div className="flex border-1 top-0">
                             <img alt="Person Icon" className="max-w-11 m-2" src="/assets/personIcon.svg" />
@@ -44,9 +58,6 @@ export const Stake = ({ }: any) => {
                      
                             <div className={`p-4 justify-self-end ${dropDownOpen? 'transform -rotate-180' : ''}`} >
                                 {svgDropDownIcon}
-                                {/* <svg width="14" height="12" viewBox="0 0 14 12" fill="none" xmlns="http://www.w3.org/2000/svg">
-                            <path d="M5.26795 0.999997C6.03775 -0.333336 7.96225 -0.333333 8.73205 1L13.0622 8.5C13.832 9.83334 12.8697 11.5 11.3301 11.5L2.66987 11.5C1.13027 11.5 0.168022 9.83333 0.937822 8.5L5.26795 0.999997Z" fill="#C1F6FF"/>
-                        </svg> */}
                             </div>
                         </div>
                     </div>
@@ -54,27 +65,27 @@ export const Stake = ({ }: any) => {
             </Disclosure.Button>
 
             <Disclosure.Panel className={`col-span-full grid flex `}>
-                <div className={`col-span-full grid flex grid-flow-col rounded border border-foreground-3 bg-gradient-to-b from-background to-background-4`} >
+                <div className={`col-span-full grid flex grid-flow-col rounded border border-foreground-3 bg-gradient-to-b from-background to-background-5`} >
                     <div className="m-2 mx-8 text-color-6 text-m  ">Amount</div>
                     <div className="m-2 grid-flow-row grid " >
                         <input className="m-2 mr-1 bg-gradient-to-t from-background-4 via-foreground-3 to-background-4"  type="number" value={inputValue} onChange={handleInputChange} />
                         <div className="grid-flow-col grid  place-content-between">
                             <div className="grid-flow-col grid w-6/12 justify-self-start">
-                                <div className="text-sm"><Button className="mx-1 text-sm" onClick={() => handleAddIncrement(5)} >5</Button></div>
-                                <div className="text-sm"><Button className="mx-1 text-sm" onClick={() => handleAddIncrement(20)}>20</Button></div>
-                                <div className="text-sm"><Button className="mx-1 text-sm" onClick={() => handleAddIncrement(125)}>125</Button></div>
+                                <div className="text-sm"><Button className="mx-1 text-sm" onClick={() => handleAddFixedValue(5)} >5</Button></div>
+                                <div className="text-sm"><Button className="mx-1 text-sm" onClick={() => handleAddFixedValue(20)}>20</Button></div>
+                                <div className="text-sm"><Button className="mx-1 text-sm" onClick={() => handleAddFixedValue(125)}>125</Button></div>
                             </div>
                             <div className="mr-20 flex grid-flow-col grid w-6/12 justify-self-end h-8">
                                 <div className="text-xs mx-1">Lockup period</div>
-                                <div className="text-xs h-8"><Button className="mx-1 text-xs h-8">3 months</Button></div>
-                                <div className="text-xs h-8"><Button className="mx-1 text-xs h-8">6 months</Button></div>
-                                <div className="text-xs h-8"><Button className="mx-1 text-xs h-8">12 months</Button></div>
+                                <div className="text-xs h-8"><Button className="mx-1 text-xs h-8" onClick={() => handleLockedPeriod(3)} >3 months</Button></div>
+                                <div className="text-xs h-8"><Button className="mx-1 text-xs h-8" onClick={() => handleLockedPeriod(6)} >6 months</Button></div>
+                                <div className="text-xs h-8"><Button className="mx-1 text-xs h-8" onClick={() => handleLockedPeriod(12)} >12 months</Button></div>
                             </div>
                         </div>
                     </div>
                 </div>
                 <div className="col-span-full grid flex my-2">
-                    <Button className="col-span-full  max-w-full flex">Stake</Button>
+                    <Button className="col-span-full  max-w-full flex" onClick={() => handleStake()} >Stake</Button>
                 </div>
             </Disclosure.Panel>
 
