a66ee6c1a4415705a8fccf15f81b510dcd4a2472
Gerald Iakobinyi-Pich1 <nutrina9@gmail.com>
2024-03-23 23:05:22 +0200

    fix: filter total amount in 'Your Stake' section by chain id

--- app/src/components/components_staking/YourStake.tsx
+++ app/src/components/components_staking/YourStake.tsx
@@ -1,19 +1,31 @@
-import React from "react";
+import React, { useMemo } from "react";
 import { YourStakeForm } from "./YourStakeForm";
 import { StakeSection } from "./StakeSection";
 import { YourStakeHistory } from "./YourStakeHistory";
 import { StakeData, useStakeHistoryQuery } from "@/utils/stakeHistory";
-import { useAccount } from "wagmi";
+import { useAccount, useChainId } from "wagmi";
 
 export const YourStake = () => {
   const { address } = useAccount();
+  const chainId = useChainId();
   const { data } = useStakeHistoryQuery(address);
-  const yourStakeHistory = data?.filter((stake: StakeData) => {
-    return stake.stakee === address?.toLowerCase();
-  });
-  const stakedAmount: string = yourStakeHistory
-    ? yourStakeHistory.reduce((acc, stake) => acc + BigInt(stake.amount), 0n).toString()
-    : "0";
+
+  const stakedAmount = useMemo(
+    () =>
+      data
+        ? data.reduce((acc, stake) => {
+            if (
+              stake.staker.toLowerCase() === address?.toLowerCase() &&
+              stake.stakee.toLowerCase() === address.toLowerCase() &&
+              stake.chain === chainId
+            ) {
+              acc += BigInt(stake.amount);
+            }
+            return acc;
+          }, 0n)
+        : 0n,
+    [data, address, chainId]
+  );
 
   return (
     <StakeSection
@@ -23,7 +35,7 @@ export const YourStake = () => {
       }}
       heading="Your Stake"
       subheading="Secure your identity by staking GTC. Higher stakes mean more trust in your passport."
-      amount={stakedAmount}
+      amount={stakedAmount.toString()}
     >
       <YourStakeForm />
       <YourStakeHistory />
