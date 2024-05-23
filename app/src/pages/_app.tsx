484ce251ea51955a71c32253babb35b77b47f553
Gerald Iakobinyi-Pich1 <nutrina9@gmail.com>
2024-05-23 10:00:46 +0300

    feat: integrating intercom widget

--- app/src/pages/_app.tsx
+++ app/src/pages/_app.tsx
@@ -14,6 +14,8 @@ import { wagmiConfig } from "@/utils/wagmi";
 
 import { datadogLogs } from "@datadog/browser-logs";
 
+const INTERCOM_APP_ID = process.env.NEXT_PUBLIC_INTERCOM_APP_ID || "";
+
 datadogLogs.init({
   clientToken: `${process.env.NEXT_PUBLIC_DATADOG_CLIENT_TOKEN}`,
   site: "us3.datadoghq.com",
@@ -39,7 +41,62 @@ const RenderOnlyOnClient = ({ children }: { children: React.ReactNode }) => {
 
 const queryClient = new QueryClient();
 
+declare global {
+  interface Window {
+    intercomSettings?: {
+      api_base: string;
+      app_id: string;
+    };
+    Intercom: any;
+  }
+}
+
 export default function App({ Component, pageProps }: AppProps) {
+  useEffect(() => {
+    if (typeof window !== "undefined") {
+      window.intercomSettings = {
+        api_base: "https://api-iam.intercom.io",
+        app_id: INTERCOM_APP_ID,
+      };
+      (function () {
+        var w: any = window;
+        var ic = w.Intercom;
+        if (typeof ic === "function") {
+          ic("reattach_activator");
+          ic("update", w.intercomSettings);
+        } else {
+          var d = document;
+          var i = function () {
+            // @ts-ignore
+            i.c(arguments);
+          };
+          // @ts-ignore
+          i.q = [];
+          // @ts-ignore
+          i.c = function (args) {
+            // @ts-ignore
+            i.q.push(args);
+          };
+          w.Intercom = i;
+          var l = function () {
+            var s = d.createElement("script");
+            s.type = "text/javascript";
+            s.async = true;
+            s.src = "https://widget.intercom.io/widget/" + INTERCOM_APP_ID;
+            var x = d.getElementsByTagName("script")[0];
+            x.parentNode?.insertBefore(s, x);
+          };
+          if (document.readyState === "complete") {
+            l();
+          } else if (w.attachEvent) {
+            w.attachEvent("onload", l);
+          } else {
+            w.addEventListener("load", l, false);
+          }
+        }
+      })();
+    }
+  }, []);
   return (
     <>
       <Head>
